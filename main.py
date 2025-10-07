6245b0da51c8e7cf67a1a81c82a28b92289d1db3
jual6332@colorado.edu <jual6332@colorado.edu>
2025-10-07 19:54:31 -0400

    Added job1 data

--- main.py
+++ main.py
@@ -9,14 +9,15 @@
 # We can answer these sorts of questions and more!
 
 # Actual coding:
-# I will start with using data entry, then will convert to sql. But long term web-scraping is the plan and to convert the useful data to sql.
+# I will start with using data entry. But long term web-scraping is the plan and to convert the useful data to sql.
 
 class JobPosting():
-	def __init__():
+	def __init__(self):
 		self.title=""
 		self.company=""
 		self.description=""
 		self.skills=[]
+		self.highlight=""
 	def get_title(self):
 		return self.title
 	def get_company_name(self):
@@ -25,6 +26,8 @@ class JobPosting():
 		return self.description
 	def get_skills(self):
 		return self.skills
+	def get_highlight(self):
+		return self.highlight
 	def set_title(self,tle):
 		self.company = tle
 	def set_company_name(self,cmp):
@@ -33,5 +36,19 @@ class JobPosting():
 		self.description = desc
 	def add_skill_to_list(self,skill):
 		self.skills.append(skill)
+	def set_highlight(self,hi):
+		self.highlight = hi
 
-
+job1 = JobPosting()
+job1.set_title("Principal Software Engineer")
+job1.set_company_name("Tyler Technologies")
+job1.add_skill_to_list("Software deployment")
+job1.add_skill_to_list("Scalable systems")
+job1.add_skill_to_list("Socalability")
+job1.add_skill_to_list("High availability")
+job1.add_skill_to_list("High availability architecture")
+job1.add_skill_to_list("Docker")
+job1.add_skill_to_list("Agile software development")
+job1.add_skill_to_list("Automation")
+job1.add_skill_to_list("Continuous Delivery")
+job1.set_highlight("Mentor and coach engineers, providing opportunities for growth and ensuring excellence in software engineering practices.Engage in high-level strategic discussions while also contributing to technical execution (80% hands-on coding, 20% strategy).")
